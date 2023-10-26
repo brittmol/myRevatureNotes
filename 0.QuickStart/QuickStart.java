46113c56b51fac7e399bf4081c0259c586dde237
Brittany Moliver <britt.moliver@gmail.com>
2023-10-25 23:12:15 -0400

    new revature folder for JAVA

---
+++ 0.QuickStart/QuickStart.java
@@ -0,0 +1,11 @@
+class QuickStart {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        int a = 3;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(--a);
+        a *= 2;
+        System.out.println(a);
+    }
+}
